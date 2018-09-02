0191e689f39030eeb2be4c68d50b521565e9c3d6
Marcos O. Alves <eng.marcos.alves@gmail.com>
2018-09-02 17:18:40 -0300

    configuração jpa, hibernate, acesso ao mysql 8

---
+++ src/main/java/br/com/geekcode/marcos/desafio/config/SpringJpaConfig.java
@@ -0,0 +1,55 @@
+package br.com.geekcode.marcos.desafio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class SpringJpaConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/db_cidades?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC");
+        ds.setUsername("Marcos");
+        ds.setPassword("Marcos#123");
+        return ds;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("br.com.geekcode.marcos.desafio.domain");
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factory.setJpaProperties(jpaProperties());
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager tx = new JpaTransactionManager();
+        tx.setEntityManagerFactory(entityManagerFactory());
+        tx.setJpaDialect(new HibernateJpaDialect());
+        return tx;
+    }
+
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.format_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        return properties;
+    }
+}
---
+++ src/main/java/br/com/geekcode/marcos/desafio/config/SpringRootConfig.java
@@ -0,0 +1,13 @@
+package br.com.geekcode.marcos.desafio.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("br.com.geekcode.marcos.desafio")
+public class SpringRootConfig {
+}
