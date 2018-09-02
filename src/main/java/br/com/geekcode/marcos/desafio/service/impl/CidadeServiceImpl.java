f6a757dded9f778c146e73c8e7d6433208eb96fb
Marcos O. Alves <eng.marcos.alves@gmail.com>
2018-09-02 18:11:58 -0300

    camada service

--- src/main/java/br/com/geekcode/marcos/desafio/service/impl/CidadeServiceImpl.java
+++ src/main/java/br/com/geekcode/marcos/desafio/service/impl/CidadeServiceImpl.java
@@ -4,9 +4,13 @@ import br.com.geekcode.marcos.desafio.dao.CidadeDao;
 import br.com.geekcode.marcos.desafio.model.Cidade;
 import br.com.geekcode.marcos.desafio.service.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class CidadeServiceImpl implements CidadeService {
 
     @Autowired
@@ -14,26 +18,39 @@ public class CidadeServiceImpl implements CidadeService {
 
     @Override
     public void save(Cidade cidade) {
-        
+        cidadeDao.save(cidade);
     }
 
     @Override
     public void update(Long ibgeId, Cidade cidade) {
-
+        cidade.setIbgeId(idValido(ibgeId));
+        cidadeDao.findById(ibgeId);
+        cidadeDao.update(cidade);
     }
+
 
     @Override
     public void delete(Long ibgeId) {
-
+        cidadeDao.delete(idValido(ibgeId));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Cidade findById(Long ibgeId) {
-        return null;
+        return cidadeDao.findById(idValido(ibgeId));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Cidade> findAll() {
-        return null;
+        return cidadeDao.findAll();
+    }
+
+    private Long idValido(Long ibgeId) {
+        if (ibgeId <= 0) {
+            throw new NotValidIdException("Valor do campo ibgeId está inválido. " +
+                    "Deve ser um valor inteiro maior que zero.");
+        }
+        return ibgeId;
     }
 }
